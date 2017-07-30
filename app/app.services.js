9087861f763b28a401f99afd8589acb14adb84be
mikejcai <mike.cai@gmail.com>
2017-07-29 20:31:03 -0700

    Add files via upload

--- app/app.services.js
+++ app/app.services.js
@@ -22,10 +22,14 @@
 		============================================*/
 		
 		function getSongMatches (song, artist) {
-			var query_url = 'http://10.11.255.25:9010/SongSelection?song_name=' + song + '&artist_name=' + artist;
-			//var query_url = 'api/query1.json';
+//			var query_url = '/SongSelection?song_name=' + song + '&artist_name=' + artist;
+//			var query_url = 'api/query1.json';
 			
-			return $http.get(query_url)
+//			return $http.get(query_url)
+			return $http({
+				  method: 'GET',
+				  url: 'SongQueryServlet',
+				  params: {song_name: song, artist_name:artist}})
 						.then(function(results) {
 							return results.data;
 						}, function(error) {
@@ -34,17 +38,22 @@
 		}
 
 		function getRecommendedSongs (refSongId, refArtistId, numRecSongs, frac) {
-			var query_url = 'http://10.11.255.25:9010/SongRecommendation?song_key=' + refSongId 
-			 	+ '&artist_key=' + refArtistId 
-			 	+ '&num_recs=' + numRecSongs 
-			 	+ '&num_same_artist=' + Math.floor(frac*numRecSongs/100)
+			//var query_url = 'http://10.2.62.101:9010/SongRecommendation?song_key=' + refSongId 
+			// 	+ '&artist_key=' + refArtistId 
+			// 	+ '&num_recs=' + numRecSongs 
+			// 	+ '&num_same_artist=' + Math.floor(frac*numRecSongs/100)
 			// 	+ '&sim_artist_param=' + simArtistParam
 			// 	+ '&user_name=' + userName 
 			// 	+ '&sim_user_param=' + simUserParam;
 
-			//var query_url = 'api/query2.json';
+			var query_url = 'api/query2.json';
 
-			return $http.get(query_url)
+//			return $http.get(query_url)
+			return $http({
+				method: 'GET',
+				url: 'SongRecServlet',
+				params: {song_key: refSongId, artist_key:refArtistId, num_recs:numRecSongs, num_same_artist:Math.floor(frac*numRecSongs/100)}
+			})
 						.then(function(results) {
 							console.log(refSongId, refArtistId, numRecSongs, frac);
 							return results.data;
